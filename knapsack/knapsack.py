ceb0b26c9205bdd284d9b52a03db622fbbdb4da4
DentonWarnock <dentonwarnock@gmail.com>
2020-08-11 16:15:21 -0700

    added plan and code for knapsack.py. 3 tests failed. need to optimize solution.

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -6,9 +6,34 @@ from collections import namedtuple
 Item = namedtuple('Item', ['index', 'size', 'value'])
 
 def knapsack_solver(items, capacity):
-    # Your code here
-
-    pass
+    # create empty sack to add items to and return
+    sack = []
+    
+    # sort items by value
+    items.sort(key=lambda x: x.value, reverse=True) # sort by most valuable items first
+    
+    # put most valueable items in sack until full
+    weight = 0
+    value = 0
+    for item in items:   
+      print(f'starting weight: {weight}')    
+      print(item)  
+      print(item.size)
+      # try adding the weight
+      weight += item.size
+      print(f'ending weight: {weight}')
+      # check if the new weight will be too heavy - if so return early
+      if weight > capacity:
+        return {'Value': value, 'Chosen': sack, "Weight": weight}
+      else:
+        # add items value and index        
+        value += item.value
+        sack.append(item.index)
+    
+    return {'Value': value, 'Chosen': sack}
+    
+    
+    
 
 
 if __name__ == '__main__':
