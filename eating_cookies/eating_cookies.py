4e5ace7b0689eda8a42155993a0c7993cf18ea0d
DentonWarnock <dentonwarnock@gmail.com>
2020-08-12 20:30:32 -0700

    added eating_cookies.py

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,12 +3,18 @@ Input: an integer
 Returns: an integer
 '''
 def eating_cookies(n):
-    # Your code here
-
-    pass
+    # base case
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    else:
+        # make recursive call
+        return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+    
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 10
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
